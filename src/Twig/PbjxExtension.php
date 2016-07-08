6038ae6b0af857c361c9a5c79b90781f21245c7b
Greg Brown <greg@12dnetworks.com>
2016-07-08 00:36:29 -0700

    add pbj_template method in twig

---
+++ src/Twig/PbjxExtension.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Gdbots\Bundle\PbjxBundle\Twig;
+
+use Gdbots\Pbj\Message;
+
+class PbjxExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('pbj_template', [$this, 'pbjTemplate'])
+        ];
+    }
+
+    /**
+     * Returns a reference to a twig template based on the schema of the provided message (pbj schema).
+     * This allows for component style development for pbj messages.  You are asking for a template that
+     * can render your message (e.g. Article) as a "card", "modal", "slack-post", etc. and optionally that
+     * template can be device view specific. (card.smartphone.html.twig)
+     *
+     * Example:
+     *  {% include pbj_template(pbj, 'card', device_view) with {'pbj': pbj} %}
+     *
+     * @param Message $pbj
+     * @param string $template
+     * @param string $deviceView
+     * @param string $format
+     *
+     * @return string|string[]  A single template reference or array if device view is not null
+     */
+    public function pbjTemplate(Message $pbj, $template, $deviceView = null, $format = 'html')
+    {
+        // todo: memoize?
+        $curieStr = $pbj::schema()->getCurie()->toString();
+        $default = sprintf('@%s/%s.%s.twig', str_replace(':', '/', $curieStr), $template, $format);
+
+        if (null === $deviceView) {
+            return $default;
+        }
+
+        return [
+            sprintf('@%s/%s.%s.%s.twig', str_replace(':', '/', $curieStr), $template, $deviceView, $format),
+            $default,
+        ];
+    }
+
+    /*
+     * todo: add pbjx_request twig function... i.e. pbjx_request(curie, data)
+     *
+     * {% set pbjx_response = pbjx_request(curie, data) %}
+     * {% if pbjx_response %}
+     *   {% include pbj_template(pbjx_response, 'card', device_view) with {'pbj': pbjx_response} %}
+     * {% endif %}
+     */
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'gdbots_pbjx_extension';
+    }
+}
