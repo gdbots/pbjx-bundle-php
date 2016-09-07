72d7f049e0ccefb575a95d6bf81bdbfb148ff60b
Michael H. Arieli <michaelharieli@gmail.com>
2016-09-07 14:47:15 -0700

    Removed post submit event - not supporting doctrine collection

--- src/Form/EventListener/CollectionTypeSubscriber.php
+++ src/Form/EventListener/CollectionTypeSubscriber.php
@@ -2,7 +2,6 @@
 
 namespace Gdbots\Bundle\PbjxBundle\Form\EventListener;
 
-use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -16,30 +15,8 @@ class CollectionTypeSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            FormEvents::POST_SUBMIT => 'postSubmit',
             FormEvents::PRE_SUBMIT  => 'preSubmit'
         );
-    }
-
-    /**
-     * Removes empty collection elements.
-     *
-     * @param FormEvent $event
-     */
-    public function postSubmit(FormEvent $event)
-    {
-        /** @var Collection $items */
-        $items = $event->getData();
-
-        if (!$items || !$items instanceof Collection) {
-            return;
-        }
-
-        foreach ($items as $item) {
-            if ($this->isEmpty($item)) {
-                $items->removeElement($item);
-            }
-        }
     }
 
     /**
